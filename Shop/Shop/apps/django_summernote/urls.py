8a775680668d897f68a3380bf1290526fc44706b
TheProgersTeam <68988841+ValeryChapman@users.noreply.github.com>
2021-05-27 22:33:52 +0300

    nc

--- Shop/Shop/apps/django_summernote/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import SummernoteEditor, SummernoteUploadAttachment
-
-urlpatterns = [
-    path('editor/<str:id>/', SummernoteEditor.as_view(), name='django_summernote-editor'),
-    path('upload_attachment/', SummernoteUploadAttachment.as_view(), name='django_summernote-upload_attachment'),
-]
